812389ad4fa627b82dfd75b4db3267dc59728fc4
Rudolf Tammekivi <rtammekivi@gmail.com>
2013-01-18 19:22:55 +0200

    u8800: Initial list of hardware-specific features.
    This list will be updated when new feature is added into the ROM.

--- device.mk
+++ device.mk
@@ -35,6 +35,12 @@ DEVICE_PACKAGE_OVERLAYS += device/huawei/u8800/overlay
 PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
+# Hardware-specific features
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
 # Init scripts
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/init.emmc.rc:root/init.emmc.rc \
