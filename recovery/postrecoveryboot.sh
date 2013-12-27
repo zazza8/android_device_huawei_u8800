77202fd249145150153b7d632269891f5b08923b
Rudolf Tammekivi <rtammekivi@gmail.com>
2013-12-27 11:35:02 +0200

    u8800: Fix framebuffer alignment in recovery.

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,5 +1,8 @@
 #!/sbin/sh
 
+# Disable frambuffer alignment
+echo 0 > /sys/module/msm_fb/parameters/align_buffer
+
 # Set brightnes for LCD and buttons
 echo 127 > /sys/class/leds/lcd-backlight/brightness
 echo 127 > /sys/class/leds/button-backlight/brightness
