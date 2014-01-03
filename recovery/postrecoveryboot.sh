7cb7af1085e7d2ff9bc14115e148cb76efcd5bc2
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-01-03 13:21:27 +0200

    u8800: Stop messing with USB in recovery.

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -6,8 +6,3 @@ echo 0 > /sys/module/msm_fb/parameters/align_buffer
 # Set brightnes for LCD and buttons
 echo 127 > /sys/class/leds/lcd-backlight/brightness
 echo 127 > /sys/class/leds/button-backlight/brightness
-
-# Enable USB Mass Storage
-echo 0 > /sys/class/android_usb/android0/enable
-echo adb,mass_storage > /sys/class/android_usb/android0/functions
-echo 1 > /sys/class/android_usb/android0/enable
