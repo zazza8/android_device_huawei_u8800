c12d05ac7687f0b969a76c587a497c5ef9c1f56d
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-01-26 18:57:24 +0200

    camera: Include proper media headers.
    CM uses different media HALs, and since we are using CAF HAL, the path must be
    updated.

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -31,7 +31,7 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_C_INCLUDES += \
     hardware/qcom/display-$(TARGET_QCOM_DISPLAY_VARIANT)/libgralloc \
-    hardware/qcom/media/libstagefrighthw
+    hardware/qcom/media-$(TARGET_QCOM_DISPLAY_VARIANT)/libstagefrighthw
 
 LOCAL_SHARED_LIBRARIES := \
     libbinder \
