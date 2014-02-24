d546474eb64a0c8322c6bfabc257c9e822b421b0
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-02-24 12:22:51 +0200

    lights: Set attention & notification as on top of the priority list.

--- liblights/lights.c
+++ liblights/lights.c
@@ -207,10 +207,12 @@ set_speaker_light_locked(struct light_device_t* dev,
 static void
 handle_speaker_battery_locked(struct light_device_t* dev)
 {
-    if (is_lit(&g_battery)) {
-        set_speaker_light_locked(dev, &g_battery);
-    } else {
+    if (is_lit(&g_attention)) {
+        set_speaker_light_locked(dev, &g_attention);
+    } else if (is_lit(&g_notification)) {
         set_speaker_light_locked(dev, &g_notification);
+    } else {
+        set_speaker_light_locked(dev, &g_battery);
     }
 }
 
